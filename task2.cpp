ac4a601803a268f773481b0431ac1d2dd4505be6
mmaneh <mmanemkhitaryan@gmail.com>
2025-08-17 23:15:18 +0400

    second part of task

--- task2.cpp
+++ task2.cpp
@@ -2,12 +2,13 @@
 #include "myTraits.hpp"
 
 //task 4: error
-/*#define PRINT_TYPE_INFO(T) {
+#define PRINT_TYPE_INFO(T) {
 	if constexpr (my_traits::is_integral<T>::value) std::cout << "type is integral" << std::endl;
 	else if constexpr (my_traits::is_floating_point<T>::value) std::cout << "type is floating point" << std::endl;
         else if constexpr (my_traits::is_pointer<T>::value) std::cout << "type is pointer" << std::endl;
         else if constexpr (my_traits::is_array<T>::value) std::cout << "type is array" << std::endl;
-}*/
+}
+
 //task 1
 template <typename T>
 typename my_traits::enable_if<my_traits::is_integral<T>::value, T>::type sum(T a, T b) {
@@ -42,8 +43,8 @@ int main() {
 	auto x = choose_type<my_traits::is_integral<int>::value, int, double>();
 	std::cout << my_traits::is_same<decltype(x),int>::value;
 	std::cout << my_traits::is_same<base_type<const int&>::type, int>::value;
-//	PRINT_TYPE_INFO(int*);
-//	PRINT_TYPE_INFO(int[]);
+	PRINT_TYPE_INFO(int*);
+	PRINT_TYPE_INFO(int[]);
 	
 
 }
